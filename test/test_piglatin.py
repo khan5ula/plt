6896f27e8b9a88b35911d384e02eec7c7da9a053
Kristian Hannula <kristian.hannula@proton.me>
2024-10-31 16:56:16 +0200

    story 1

--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -4,6 +4,6 @@ from error import PigLatinError
 
 
 class TestPigLatin(unittest.TestCase):
-
-    def test_something(self):
-        pass
+    def test_get_phrase(self):
+        translator = PigLatin("phrase")
+        self.assertEqual("phrase", translator.get_phrase())
