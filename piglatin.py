6b98473b1ed97f9608f488f4d135ac23894bbc49
Kristian Hannula <kristian.hannula@proton.me>
2024-10-31 17:26:45 +0200

    story 3

--- piglatin.py
+++ piglatin.py
@@ -8,3 +8,23 @@ class PigLatin:
     def translate(self) -> str:
         if not self._phrase:
             return "nil"
+
+        if self._starts_with_vowel(self._phrase):
+            return self._handle_phrase_starting_vowel(self._phrase)
+
+    def _starts_with_vowel(self, phrase: str) -> bool:
+        if phrase:
+            return phrase.lower()[0] in ("a", "e", "i", "o", "u", "y")
+
+    def _ends_with_vowel(self, phrase: str) -> bool:
+        if phrase:
+            return phrase.lower()[len(phrase) - 1] in ("a", "e", "i", "o", "u", "y")
+
+    def _handle_phrase_starting_vowel(self, phrase: str) -> str:
+        if phrase:
+            if phrase[len(phrase) - 1] == "y":
+                return phrase + "nay"
+            if self._ends_with_vowel(phrase):
+                return phrase + "yay"
+            else:
+                return phrase + "ay"
--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -11,3 +11,15 @@ class TestPigLatin(unittest.TestCase):
     def test_translate_empty_string(self):
         translator = PigLatin("")
         self.assertEqual("nil", translator.translate())
+
+    def test_phrase_starting_with_vowel(self):
+        translator_nay = PigLatin("yay")
+        translator_yaa = PigLatin("yaa")
+        translator_yan = PigLatin("yan")
+
+        print(translator_nay.translate())
+        print(translator_yaa.translate())
+        print(translator_yan.translate())
+        self.assertEqual(translator_nay.translate(), "yaynay")
+        self.assertEqual(translator_yaa.translate(), "yaayay")
+        self.assertEqual(translator_yan.translate(), "yanay")
